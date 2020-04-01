fac029a577ded5895f18eb0fcaa44d407003995c
Leo Gou <leo_g92@hotmail.fr>
2020-04-01 12:48:56 +0200

    Seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+istanbul = Restaurant.create(name: 'Istanbul', category: 'belgium', address: 'LGC')
+
+brume = Restaurant.create(name: 'Brume', category: 'french', address: 'Rennes')
+
+mcdo = Restaurant.create(name: 'Mcdo', category: 'belgium', address: 'New York')
+
+lucca = Restaurant.create(name: 'Lucca', category: 'italian', address: 'Rennes')
+
+chez_oam = Restaurant.create(name: 'Chez oam!', category: 'chinese', address: "Au-dessus c'est le Soleil!")
